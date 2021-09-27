eccee2b35479c09814e372a8d31418e9c9d63411
Bayu Nurkholis <31134993+pinokio07@users.noreply.github.com>
2021-09-27 15:01:36 +0700

    Update UserImport.php

--- app/Imports/UserImport.php
+++ app/Imports/UserImport.php
@@ -22,10 +22,11 @@ class UserImport implements ToCollection
                       'kelas' => $row[4],
                       'role' => $row[5]
                     ]);
+            //Start Edit Here
             if($user->password == ''){
               $user->password = bcrypt('rahasia');
             }
-            
+            //End Edit Here
           }
         }
     }
