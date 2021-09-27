5675d5b0567dc09679a1b268f07097aa38a4f52e
Bayu Nurkholis <31134993+pinokio07@users.noreply.github.com>
2021-09-27 15:21:03 +0700

    Update First Or Create

--- app/Imports/UserImport.php
+++ app/Imports/UserImport.php
@@ -14,18 +14,15 @@ class UserImport implements ToCollection
     {
         foreach ($collection as $key => $row) {
           if($key > 0 && $row[1] != ''){
-            $user = \App\Models\User::updateOrCreate([
+            $user = \App\Models\User::firstOrCreate([
                       'nis' => $row[1]
                     ],[
                       'nama' => $row[2],
                       'jenis_kelamin' => $row[3],
                       'kelas' => $row[4],
-                      'role' => $row[5]
+                      'role' => $row[5],
+                      'password' => bcrypt('rahasia')
                     ]);
-            if($user->password == ''){
-              $user->password = bcrypt('rahasia');
-            }
-            
           }
         }
     }
