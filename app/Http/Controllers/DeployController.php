22aeaf33cac81518deaa3ec93018403fe9120716
Bayu Nurkholis <31134993+pinokio07@users.noreply.github.com>
2022-03-22 16:20:23 +0700

    Update Deploy Controller

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class DeployController extends Controller
 {
@@ -13,7 +15,16 @@ class DeployController extends Controller
       $localToken = config('app.deploy_secret');
       $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
       if (hash_equals($githubHash, $localHash)) {
-            return "OK";
+        $proses = new Process('git pull');
+        $proses->setWorkingDirectory(base_path());
+
+        $proses->run();
+
+        if($proses->isSuccessful()){
+            //...
+        } else {
+            throw new ProcessFailedException($proses);
+        }
       }
     }
 }
