4e201520f45ff9467e4a4c8cb26b0a92df0df6b2
Bayu Nurkholis <31134993+pinokio07@users.noreply.github.com>
2022-03-23 09:44:08 +0700

    Update Webhook Deploy

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -10,24 +10,36 @@ class DeployController extends Controller
 {
     public function index(Request $request)
     {
-      $githubPayload = $request->getContent();
-      $githubHash = $request->header('X-Hub-Signature');
-      $localToken = config('app.deploy_secret');
-      $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
-      if (hash_equals($githubHash, $localHash)) {
-        $gitUser = config('app.git_username');
-        $gitPass = config('app.git_password');
+      // $githubPayload = $request->getContent();
+      // $githubHash = $request->header('X-Hub-Signature');
+      // $localToken = config('app.deploy_secret');
+      // $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
+      // if (hash_equals($githubHash, $localHash)) {
 
-        $proses = new Process('git pull https://'.$gitUser.':'.$gitPass.'@github.com/pinokio07/hotel_fadilah');
-        $proses->setWorkingDirectory(base_path());
+        $abc = $request->sha;
 
-        $proses->run();
-
-        if($proses->isSuccessful()){
-            return "OK";
-        } else {
-            throw new ProcessFailedException($proses);
+        if(!$abc){
+          return "FAILED";
         }
-      }
+
+        $localToken = config('app.git_deploy_secret');
+
+        if($abc === $localToken){
+          $gitUser = config('app.git_username');
+          $gitPass = config('app.git_password');
+  
+          $proses = new Process('git pull https://'.$gitUser.':'.$gitPass.'@github.com/pinokio07/hotel_fadilah');
+          $proses->setWorkingDirectory(base_path());
+  
+          $proses->run();
+  
+          if($proses->isSuccessful()){
+              return "OK";
+          } else {
+              throw new ProcessFailedException($proses);
+          }
+        }
+        
+      // }
     }
 }
